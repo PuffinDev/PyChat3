6de28d10266f2e1eea0f217804fb902cd2f4a92b
Dean Jennings <puffin.develop@gmail.com>
2021-02-15 14:54:34 +0000

    Add update.sh for auto updating

---
+++ update.sh
@@ -0,0 +1,19 @@
+#PyChat auto update script
+#Run with "sudo bash update.sh"
+
+mv resources/server/accounts.json ..
+mv resources/server/admins.txt ..
+mv resources/server/banned.txt ..
+mv resources/client/config.json ..
+
+cd ..
+sudo rm -rf PyChat
+git clone https://github.com/PuffinDev/PyChat.git
+cd PyChat
+
+mv ../accounts.json resources/server
+mv ../admins.txt resources/server
+mv ../banned.txt resources/server 
+mv ../config.json resources/client
+
+echo PyChat is now updated!
